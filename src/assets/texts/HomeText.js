6125fb16076f8821da7883940c1a19af7b6042f0
snub_yeah <luke.wilson@my.okcu.edu>
2024-08-02 00:49:16 -0500

    Added cool hotline miami head spinny thing

--- src/assets/texts/HomeText.js
+++ src/assets/texts/HomeText.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from 'react';
 import './TextBox.css';
 
 const HomeText = () => {
+    const [currentSentenceIndex, setCurrentSentenceIndex] = useState(0);
+    const sentences = [
+        "Welcome to my site...",
+        "I'm a CS student...",
+        "...",
+        "...kinda"
+    ];
+
+    const handleClick = () => {
+        setCurrentSentenceIndex((prevIndex) => (prevIndex + 1) % sentences.length);
+    };
+
     const containerStyle = {
         width: '100%',
         backgroundColor: '#000000', 
@@ -14,16 +26,28 @@ const HomeText = () => {
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'flex-end',
-        height: '100vh'
+        height: '100vh',
+        position: 'relative'
+    };
+
+    const imageStyle = {
+        position: 'absolute',
+        right: '0',
+        top: '10%',
+        transform: 'translateY(-0%)',
+        width: '600px', 
+        height: 'auto'
     };
 
     return (
-        <div style={wrapperStyle}>
-            <div style={containerStyle}>
-                <p>M. Bison Missle Inbound!</p>
+        <div style={wrapperStyle} onClick={handleClick}>
+            <div className='click-to-continue'>CLICK TO CONTINUE</div>
+            <div style={containerStyle} className="text-container">
+                <p>{sentences[currentSentenceIndex]}</p>
             </div>
+            <img src="/images/richard.gif" alt="Animated GIF" style={imageStyle} className="rotating-image" />
         </div>
-    )
-}
+    );
+};
 
 export default HomeText;
